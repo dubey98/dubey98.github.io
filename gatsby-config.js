1a91b47f520572f61f935bd464ee51985d696f53
Shiv Dubey <shivdubey.dev@gmail.com>
2021-09-12 19:25:49 +0530

    designed front page and image responsiveness added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,23 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "my-folio",
   },
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,36 @@
 import React from "react";
-import "./../styles/global.css";
+import Layout from "../components/Layout";
+
+import ProjectPanel from "../components/ProjectPanel";
 
 function index() {
   return (
-    <div class="mx-auto flex flex-col bg-red-100 h-screen text-red-800">
-      <header class="container flex-0 mx-auto text-opacity-100">
-        <nav class="flex text-center uppercase items-center h-10 font-light">
-          <div class="px-3 flex-0">Home</div>
-          <div class="px-3 flex-0">about</div>
-          <div class="px-3 flex-1 font-bold text-2xl">Shiv DUbey</div>
-          <div class="px-3 flex-0">proects</div>
-          <div class="px-3 flex-0">contact</div>
-        </nav>
-      </header>
-      <main class="flex-1 flex bg-gradient-to-r from-red-100 to-red-200 ">
-        <div class="flex-1 flex items-center justify-center">
-          <div class="flex-0">
-            <span class="text-5xl font-semibold">HI!!</span> <br />
-            <span class="text-4xl">
-              I'm <span class="font-semibold uppercase">Shiv Dubey</span>{" "}
-            </span>
-            <br />
-            <span class="text-xl uppercase">
-              A passionate full stack developer.
-            </span>
+    <Layout>
+      <main className="flex-1 block">
+        <section className="grid sm:grid-cols-2 grid-cols-1 h-screen">
+          <div className="flex items-center justify-center pl-2">
+            <div className="flex-0 leading-8">
+              <span className="text-5xl font-semibold">HI!!</span> <br />
+              <span className="text-4xl">
+                I'm <span className="font-semibold uppercase">Shiv Dubey</span>
+              </span>
+              <br />
+              <span className="text-xl uppercase">
+                A passionate full stack developer.
+              </span>
+            </div>
           </div>
-        </div>
-        <div class="flex-1 bg-developer"></div>
+          <div className="bg-developer"></div>
+        </section>
+        <section
+          className="container sm:mx-auto mx-1 flex flex-col"
+          id="projects"
+        >
+          <div className="text-5xl uppercase font-semibold py-2">#Projects</div>
+          <ProjectPanel />
+        </section>
       </main>
-      <footer class="flex-0"></footer>
-    </div>
+    </Layout>
   );
 }
 
