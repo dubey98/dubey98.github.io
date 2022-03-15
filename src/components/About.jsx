2a2d837393e38f8312eb03961f21ac3958d83205
Shiv Dubey <shiv.dubey@gaadi.com>
2022-03-15 13:39:44 +0530

    updated about info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,20 +4,24 @@ function About() {
   return (
     <div className="bg-red-300 flex flex-col justify-center h-screen ">
       <div className="text-red-800">
-        <div class="mx-auto w-4/5 max-w-4xl p-2">
-          <span class="text-4xl uppercase">About me</span> <br />
-          <div class="p-3 pl-0 italic font-serif">
-            <div class="">First, solve the problem. Then, write the code.</div>
+        <div className="mx-auto w-4/5 max-w-4xl p-2">
+          <span className="text-4xl uppercase">About me</span> <br />
+          <div className="italic font-serif mt-2 text-lg">
+            I am a full stack developer and problem solver. i have a passion for
+            building things that work and scale. i have worked in close relation
+            with clients, understanding their expectations and making it a
+            reality.
+            <div className="p-1 my-2 italic text-sm border-red-400 border-2 border-l-2 text-center">
+              First, solve the problem. Then, write the code.
+            </div>
+            i love working on problems and trying to achieve the best solution
+            possible. i believe softwares are written with pencils and a lot of
+            work is cut out for erasers.
           </div>
-          <span class="italic">
-            {" "}
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Consequatur eos rerum voluptas. Similique rem, nisi consectetur,
-            molestiae delectus, aliquid consequatur fugiat quae qui doloremque
-            voluptatum velit veritatis iste voluptates eaque?{" "}
-          </span>
-          <div class="p-2 pl-0">
-            <span class="font-semibold">Technologies i have worked with:</span>
+          <div className="p-2 pl-0">
+            <span className="font-semibold">
+              Technologies i have worked with:
+            </span>
             <br />
             <span className="text-lg">
               <i className="fab fa-react pr-1">
@@ -31,7 +35,9 @@ function About() {
           </div>
         </div>
         <div className="mx-auto w-fit pt-6" id="resume">
-          <button className="border-2 border-red-100 p-2">Download Resume</button>
+          <button className="border-2 border-red-100 p-2">
+            Download Resume
+          </button>
         </div>
       </div>
     </div>
