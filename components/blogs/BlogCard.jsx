62ffec4d5e615883665c3e51564427b8ec412ee3
Shiv Dubey <shivdubey.dev@gmail.com>
2022-07-14 22:13:43 +0530

    added tailwind typography package and styling of lists changed

--- components/blogs/BlogCard.jsx
+++ components/blogs/BlogCard.jsx
@@ -4,57 +4,54 @@ import Link from "next/link";
 
 function BlogCard({ blog }) {
   return (
-    <div className="rounded-lg bg-custom-charcoal p-4">
-      <div className="flex">
-        <div className="flex items-center justify-center">
-          <div className="mx-2 h-10 w-10 overflow-hidden rounded-full bg-custom-green relative">
-            <Image
-              src={blog.authorImageSrc}
-              alt={blog.author + " profile image"}
-              layout="fill"
-              objectFit="contain"
-            />
+    <div className="border-b border-custom-peri">
+      <div className="rounded-lg px-4 py-2">
+        <div className="flex">
+          <div className="flex items-center justify-center">
+            <div className="mx-2 h-8 w-8 overflow-hidden rounded-full bg-custom-green relative">
+              <Image
+                src={blog.authorImageSrc}
+                alt={blog.author + " profile image"}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+          </div>
+          <div className="flex-grow pl-2">
+            <h3 className="">{blog.author}</h3>
+            <p className="text-xs">{blog.datePublished}</p>
+          </div>
+          <div className="flex items-center pr-2">
+            <button>
+              <i className="fas fa-share-square"></i>
+            </button>
           </div>
         </div>
-        <div className="flex-grow pl-2">
-          <h3 className="text-lg font-medium">{blog.author}</h3>
-          <p className="text-xs">{blog.datePublished}</p>
-        </div>
-        <div className="flex items-center pr-2">
-          <button>
-            <i className="fas fa-share-square"></i>
-          </button>
-        </div>
-      </div>
-      <div className="pl-16">
-        <h1 className="py-1 sm:py-4 text-2xl sm:text-3xl font-semibold text-custom-peri">
-          <Link href={"/blogs/" + blog.slug}>
-            <a>{blog.title}</a>
-          </Link>
-        </h1>
-        <div className="flex text-sm sm:text-base flex-wrap -ml-1">
-          {blog.tags.map((tag, index) => {
-            return (
-              <button className="border-1 m-1 rounded-xl border px-2" key={index}>
-                #{tag.displayValue}
-              </button>
-            );
-          })}
-        </div>
-        <div className="flex justify-between pt-2">
-          <div className="flex items-center space-x-2 py-1 text-sm">
+        <div className="pl-2">
+          <h1 className="py-1 sm:py-2 text-xl text-custom-peri">
+            <Link href={"/blogs/" + blog.slug}>
+              <a>{blog.title}</a>
+            </Link>
+          </h1>
+          <div className="flex text-xs sm:text-base flex-wrap -ml-2">
+            {blog.tags.map((tag, index) => {
+              return (
+                <button className="mx-2 my-1 rounded-xl" key={index}>
+                  # {tag.displayValue}
+                </button>
+              );
+            })}
+          </div>
+          <div className="flex justify-between pt-2">
+            {/* <div className="flex items-center space-x-2 py-1 text-sm"> */}
             <div className="inline-block">
-              <i className="fas fa-heart px-1"></i> {blog.numOfLikes} Likes
+              <i className="fas fa-heart pr-2"></i> {blog.numOfLikes} Likes
             </div>
             <div className="inline-block">
-              <i className="fas fa-comments px-1"></i>
+              <i className="fas fa-comments px-2"></i>
               {blog.numOfComments} Comments
             </div>
-          </div>
-          <div className="box-border text-sm sm:text-base">
-            <button className="rounded-md border-2 border-custom-green px-4 py-1">
-              Save
-            </button>
+            {/* </div> */}
           </div>
         </div>
       </div>
--- pages/blogs/[slug].jsx
+++ pages/blogs/[slug].jsx
@@ -1,38 +1,37 @@
 import React from "react";
 import { getBlogBySlug, getBlogSlugs } from "../../lib/api";
-import markdownStyles from "./markdown.module.scss";
 
-function Blog({ blog, data }) {
+function Blog({ blog }) {
   return blog ? (
     <div className="bg-custom-black text-custom-green min-h-screen">
       <div className="px-4 py-12 max-w-4xl mx-auto">
         <div className="flex">
           <div className="flex items-center justify-center">
-            <div className="relative h-10 w-10 overflow-hidden rounded-full bg-custom-green"></div>
+            <div className="relative h-8 w-8 overflow-hidden rounded-full bg-custom-green"></div>
           </div>
           <div className="ml-2 flex-grow pl-2">
-            <h3 className="text-lg font-medium">{blog.author}</h3>
+            <h3 className="font-medium">{blog.author}</h3>
             <p className="text-xs">{blog.datePublished}</p>
           </div>
         </div>
-        <h1 className="py-4 text-5xl font-semibold text-custom-peri">
+        <h1 className="py-4 text-4xl font-semibold text-custom-peri">
           {blog.title}
         </h1>
-        <div className="-ml-1 flex pb-2">
+        <div className="-ml-1 flex pb-2 flex-wrap">
           {blog.tags.map((tag, index) => {
             return (
               <div
-                className="mx-1 rounded-lg border border-black px-2 bg-custom-green text-custom-black"
+                className="m-1 rounded-lg border border-black px-2 bg-custom-green text-custom-black"
                 key={index}
               >
-                {tag.displayValue}
+                #{tag.displayValue}
               </div>
             );
           })}
         </div>
         <div className="pt-6">
           <div
-            className={markdownStyles["markdown"]}
+            className="prose"
             dangerouslySetInnerHTML={{ __html: blog.content }}
           ></div>
         </div>
